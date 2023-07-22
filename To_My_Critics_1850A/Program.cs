56bec1a4641ed4112aafba0e57638a39b9e6c312
redwan-hossain <redwan.ping@gmail.com>
2023-07-22 11:23:59 +0600

    added 1850A

---
+++ To_My_Critics_1850A/Program.cs
@@ -0,0 +1,16 @@
+﻿var totalTestCase = int.Parse(Console.ReadLine()!);
+while (totalTestCase != 0)
+{
+    var testCase = Console.ReadLine()!.Split(" ");
+
+    var firstDigit = int.Parse(testCase[0]);
+    var secondDigit = int.Parse(testCase[1]);
+    var thirdDigit = int.Parse(testCase[2]);
+
+    if (firstDigit + secondDigit >= 10 || firstDigit + thirdDigit >= 10 || secondDigit + thirdDigit >= 10)
+        Console.WriteLine("YES");
+    else
+        Console.WriteLine("NO");
+
+    --totalTestCase;
+}
